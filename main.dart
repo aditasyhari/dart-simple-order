49cf143c4183c742054874cd2d9e2d74c2d33929
aditasyhari <adit.asyhari@gmail.com>
2021-12-28 03:28:27 +0700

    three

--- main.dart
+++ main.dart
@@ -3,20 +3,54 @@ import 'food.dart';
 import 'ride.dart';
 import 'send.dart';
 
+class Profile{
+  String _nama, _notelp;
+  var _alamat;
+
+  String getNama(){
+    return this._nama;
+  }
+
+  void setNama(String nama){
+    this._nama = nama;
+  }
+
+  String getAlamat(){
+    return this._alamat;
+  }
+
+  void setAlamat(String alamat){
+    this._alamat = alamat;
+  }
+
+  String getNoTelp(){
+    return this._notelp;
+  }
+
+  void setNoTelp(String notelp){
+    this._notelp = notelp;
+  }
+}
+
 void main() {
   bool ulang = true;
 
-  print("Masukkan Nama : ");
-  String nama = stdin.readLineSync();
+  var user = new Profile();
+  user.setNama("Adit");
+  user.setAlamat("Temuguruh, Sempu");
+  user.setNoTelp("087869415384");
 
-  print("Masukkan Alamat : ");
-  String alamat = stdin.readLineSync();
+  // print("Masukkan Nama : ");
+  // String nama = stdin.readLineSync();
 
-  print("Masukkan No. Telp : ");
-  String notelp = stdin.readLineSync();
+  // print("Masukkan Alamat : ");
+  // String alamat = stdin.readLineSync();
+
+  // print("Masukkan No. Telp : ");
+  // String notelp = stdin.readLineSync();
 
   do {
-    menu(nama, alamat, notelp);
+    menu(user.getNama(), user.getAlamat(), user.getNoTelp());
     stdout.write("\nApakah anda mau keluar? (y/t): ");
     String jawaban = stdin.readLineSync();
 
@@ -26,7 +60,7 @@ void main() {
 
 void menu(nama, alamat, notelp) {
   final line = "=====================================";
-  var cetakPilihanMenu = '\nPilih Menu Aplikasi \n$line\n1. Food \n2. Ride \n3. Send\n'+line;
+  var cetakPilihanMenu = '\nPilih Menu Aplikasi \n$line\n1. Food \n2. Ride \n3. Send\n4. Edit Profile\n'+line;
   print(cetakPilihanMenu);
 
   print("Enter Number :");
@@ -50,6 +84,9 @@ void menu(nama, alamat, notelp) {
         var send = new Send(nama, alamat, notelp);
         send.goSend();
       }
+      break;
+    case 4:
+      {}
       break;
     default:
       {
